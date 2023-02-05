56fe2d8eaa635a34eb085af6c75410d1d3b585e0
almerk <almerk.work@gmail.com>
2023-02-05 14:24:02 +0300

    added crud operations to controller

--- src/WeatherForecast.API/Controllers/WeatherForecastController.cs
+++ src/WeatherForecast.API/Controllers/WeatherForecastController.cs
@@ -13,9 +13,33 @@ public class WeatherForecastController : ControllerBase
         _service = service;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet(Name = "GetWeatherForecasts")]
     public async Task<IEnumerable<Models.WeatherForecast>> GetAsync(CancellationToken cancellationToken)
     {
        return await _service.GetAsync(cancellationToken);
     }
+
+    [HttpGet("{id}", Name = "GetWeatherForecast")]
+    public async Task<Models.WeatherForecast> GetAsync(string id, CancellationToken cancellationToken)
+    {
+       return await _service.GetAsync(id, cancellationToken);
+    }
+
+    [HttpPost(Name = "AddWeatherForecast")]
+    public async Task<string> AddAsync(CancellationToken cancellationToken)
+    {
+        return await _service.AddAsync(cancellationToken);
+    }
+
+    [HttpPut("{id}", Name = "UpdateWeatherForecastFull")]
+    public async Task<Models.WeatherForecast> UpdateAsync(string id, CancellationToken cancellationToken)
+    {
+        return await _service.UpdateAsync(id, cancellationToken);
+    }
+
+    [HttpDelete("{id}", Name = "DeleteWeatherForecast")]
+    public async Task UdpateAsync(string id, CancellationToken cancellationToken)
+    {
+        await _service.DeleteAsync(id, cancellationToken);
+    } 
 }
