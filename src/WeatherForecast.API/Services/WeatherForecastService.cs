876c99c91cb3dbf467d2bcfa6e3860c7c59ef375
almerk <almerk.work@gmail.com>
2023-02-05 13:53:51 +0300

    crud operations to wf service

--- src/WeatherForecast.API/Services/WeatherForecastService.cs
+++ src/WeatherForecast.API/Services/WeatherForecastService.cs
@@ -24,6 +24,64 @@ public class WeatherForecastService
         return _state.Values.AsEnumerable();
     }
 
+    public async Task<Models.WeatherForecast> GetAsync(string id, CancellationToken cancellationToken)
+    {
+        await Task.Delay(200, cancellationToken);
+        if (!_state.TryGetValue(id, out var result))
+            throw new InvalidOperationException($"id={id} not found");//TODO: validation
+        
+        return result;
+    }
+
+    public async Task<string> AddAsync(CancellationToken cancellationToken)
+    {
+        return await Task.Run(() => {
+            var newId = Guid.NewGuid().ToString();
+            var @new = new Models.WeatherForecast()
+            {
+                Id = newId,
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(_state.Count)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+            };
+
+            _state.TryAdd(newId, @new);
+
+            return newId;
+
+        }, cancellationToken);  
+    }
+
+    public async Task DeleteAsync(string id, CancellationToken cancellationToken)
+    {
+        await Task.Run(() => {
+
+            _state.TryRemove(id, out var _);
+        
+        }, cancellationToken);
+    }
+
+    public async Task<Models.WeatherForecast> UpdateAsync(string id, CancellationToken cancellationToken)
+    {
+        return await Task.Run(() => {
+            if (!_state.TryGetValue(id, out var previous))
+                throw new InvalidOperationException($"id={id} not found"); //TODO: validation
+
+            var update =  new Models.WeatherForecast()
+            {
+                Id = id,
+                Date = previous.Date,
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+            };
+
+            _state.TryUpdate(id, update, previous);
+
+            return update;      
+            
+        }, cancellationToken);
+    }
+
     private static Dictionary<string, Models.WeatherForecast> GetInitial()
         => Enumerable.Range(1, 5).Select(index => new Models.WeatherForecast
         {
