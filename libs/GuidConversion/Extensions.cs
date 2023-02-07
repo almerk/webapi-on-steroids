c45c0bb229998b17637b38eda6447bf024b619a5
almerk <almerk.work@gmail.com>
2023-02-07 14:05:36 +0300

    fixed compile error

--- libs/GuidConversion/Extensions.cs
+++ libs/GuidConversion/Extensions.cs
@@ -6,14 +6,14 @@ public static class Extensions
 {
     public static string ToUrlParameterString(this Guid guid)
     {
-        Span<byte> guidBytes = stackalloc byte [16];
+        Span<byte> guidBytes = stackalloc byte[16];
         Span<byte> base64Bytes = stackalloc byte[24];
 
         MemoryMarshal.TryWrite(guidBytes, ref guid);
         Base64.EncodeToUtf8(guidBytes, base64Bytes, out _, out _);
 
         Span<char> base64Chars = stackalloc char[22];
-         for (var i = 0; i < 22; i++)
+        for (var i = 0; i < 22; i++)
         {
             base64Chars[i] = base64Bytes[i] switch
             {
@@ -26,13 +26,13 @@ public static class Extensions
         return new string(base64Chars);
     }
 
-    public static bool TryParseGuidFromUrlParameterString(this string str, out Guid guid) 
-        => TrySpanFromUrlString(str ?? throw new ArgumentNullException(nameof(str)), guid);
+    public static bool TryParseGuidFromUrlParameterString(this string str, out Guid guid)
+        => TryParseSpanFromUrlString(str ?? throw new ArgumentNullException(nameof(str)), out guid);
 
     private static bool TryParseSpanFromUrlString(ReadOnlySpan<char> str, out Guid result)
     {
         Span<char> base64Chars = stackalloc char[24];
-        
+
         for (var i = 0; i < 22; i++)
         {
             base64Chars[i] = str[i] switch
@@ -42,16 +42,20 @@ public static class Extensions
                 _ => str[i]
             };
         }
-        
+
         base64Chars[22] = '=';
         base64Chars[23] = '=';
 
-        Span<byte> guidBytes = stackalloc byte [16];
+        Span<byte> guidBytes = stackalloc byte[16];
 
         if (!Convert.TryFromBase64Chars(base64Chars, guidBytes, out _))
+        {
+            result = default;
             return false;
-        
+        }
+
+
         result = new Guid(guidBytes);
         return true;
-    } 
+    }
 }
